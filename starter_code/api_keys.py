c0b7784e75c6f39ec77627cab50f21e9df3fb228
Karim Darbaki <Karimdarbaki14@gmail.com>
2020-04-18 23:54:16 -0500

    removed api keys from repo

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-api_key = "6b5f442280b47ab662d05bcf7080b11a"
+api_key = "YOUR KEY HERE"
 
 # Google API Key
-g_key = "AIzaSyDjHq9Q3q7GvvF1ad6zF0hoU9T75a3HCnY"
+g_key = "YOUR KEY HERE"
